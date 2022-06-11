6a01d17d2f7358a1e7d86568e2578671677ea737
Atabek <atabekseidiev@gmail.com>
2022-06-11 18:46:54 +0600

    init DZ

---
+++ 06.06/script.js
@@ -0,0 +1,19 @@
+
+
+let number = ['1','2','3','4', '5', '6','7', '8', '9','10'];
+for(let numbers = 0; numbers < number; numbers +=1){
+    console.log(number[numbers]);
+}
+
+let number = [1,2,5,6, 88, 5];
+let result = 0;
+for(let numbers in number){
+    result += number[numbers];
+}
+console.log(result);
+
+
+let root = ['1','2','3','4', '5', '6','7', '8', '9','10'];
+for(let root2 =0; root*root;){
+    console.log(root[root2]);
+}
