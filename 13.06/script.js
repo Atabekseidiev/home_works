f3d7e7f5056c0a63018e5a492c67674fb81a48a0
Atabek <atabekseidiev@gmail.com>
2022-06-21 18:36:31 +0600

    init

---
+++ 13.06/script.js
@@ -0,0 +1,17 @@
+function min(a, b) {
+    return a <= b ? a : b;
+}
+console.log(min(7, 5));
+
+function meaning(a) {
+    alert()
+
+}
+meaning(10);
+
+/*
+function func() {
+     let result = (min()+meaning());
+     alert(result);
+}
+*/
