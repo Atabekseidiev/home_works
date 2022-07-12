e998ba7ee7f988f9d741a5fe916b36bad1836a8f
Atabek <atabekseidiev@gmail.com>
2022-07-12 18:31:17 +0600

    init: 09.07

--- 09.07/script.js
+++ 09.07/script.js
@@ -1,19 +1,77 @@
-const btn_1 = document.querySelector('#btn');
+/*
+
+const btn_1 = document.querySelector('#btn1');
+const btn_2 = document.querySelector('#btn2');
+const btn_3 = document.querySelector('#btn3');
 const container = document.querySelector('#container');
 
-const showNotification = () =>{
-    const element  = document.createElement('div');
-    element.className = 'notification';
-    element.innerText = 'Hello!'
-    container.append(element);
-    setTimeout( () => element.remove(), 1500);
-    setTimeout(element);
+const showNotification = (options) =>{
+        const element = document.createElement('div');
+        element.className = options.style;
+        element.classList.add(`notification`);
+        element.innerText = options.html;
+        container.append(element);
+        setTimeout(() => element.remove(), 1500);
 }
 
-btn_1.addEventListener( 'click', showNotification);
+btn_1.addEventListener( 'click',() =>{
+    showNotification({
+        html: 'info_notification',
+        style: 'info_notification'
+    });
+})
 
-btn_1.style.width = '150px';
-btn_1.style.height = '50px';
-btn_1.style.backgroundColor = 'yellow';
+btn_2.addEventListener( 'click',() =>{
+    showNotification({
+        html: 'error_notification',
+        style: 'error_notification'
+    });
+})
+
+btn_3.addEventListener( 'click',() =>{
+    showNotification({
+        html: 'success_notification',
+        style: 'success_notification'
+    });
+})
+*/
+
+
+const smile1 = document.querySelector('#img1');
+const user_verification = () => {
+    let ask = prompt('как зовут пользователя?');
+    if (ask){
+        alert(`Добро пожаловать, ${ask}`);
+        smile1.src = ('./img/1.png');
+    }
+}
+
+smile1.addEventListener('click',user_verification);
+
+
+
+const startTimer = document.querySelector('#btn1');
+const stopTimer = document.querySelector('#btn2');
+
+/*et timer = document.querySelector('second');
+
+const timerID = () => {
+    let i = 0;
+    if (i > 0){
+        i++
+    }
+    setInterval(timerID, 1000);
+}
+
+startTimer.addEventListener('dblclick', timerID);
+*/
+
+const timerID = () =>{
+    const timer = document.createElement('div');
+    timer.classList.add(`btn1`);
+    setInterval(() => timer.remove(), 1000);
+}
+
+startTimer.addEventListener('click', timerID())
 
 
